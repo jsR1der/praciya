8ad9bce0d5bcc214d363473e4bb2aa43932773c4
rocky_balboa <168601600+jsR1der@users.noreply.github.com>
2024-08-19 22:42:53 +0300

    feat: upload input layout

---
+++ src/components/upload/Upload.tsx
@@ -0,0 +1,34 @@
+import './Upload.scss'
+import {useRef, useState} from "react";
+
+function Upload() {
+    const [imageUrl, setImageUrl] = useState<string | null>(null)
+    const inputRef = useRef<HTMLInputElement | null>(null)
+
+
+    const tryUpload = (): void => {
+        const input: HTMLInputElement | null = inputRef.current;
+        if (input) {
+            input.onchange = onChange;
+            input?.click()
+        }
+        //     show some kind of popup
+    }
+
+    const onChange = () => {
+        const input: HTMLInputElement | null = inputRef.current;
+        if (input?.files?.length) {
+            setImageUrl(input.files[0].name)
+        }
+        //     show some kind of pop up
+    }
+
+    return <div className="upload-container">
+        <input ref={inputRef} id="fileUpload" accept={'image/*'} type="file" hidden={true}/>
+        <button onClick={tryUpload} onChange={onChange}>Upload</button>
+        <div>{imageUrl ? imageUrl : 'Upload your photo'}</div>
+    </div>
+
+}
+
+export default Upload;
