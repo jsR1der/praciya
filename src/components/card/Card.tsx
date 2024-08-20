ea0a83aaae15a19fa9eaeb744a58f0ba17714e3f
rocky_balboa <168601600+jsR1der@users.noreply.github.com>
2024-08-20 12:15:53 +0300

    'fix: add overflow elipsis to card items

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,12 +1,13 @@
 import './Card.scss';
+import Image from '../image/Image'
 
 function Card() {
     return <div className="grid grid-cols-1 card-container">
-            <img className="image" src="https://www.terazmuzyka.pl/wp-content/uploads/news/ja/jacksparrow_1057.jpeg" alt=""/>
-            <p className="name">Philip Johnson</p>
-            <p className="title">Some developer</p>
-            <p className="email">myEmail@gmail.com</p>
-            <p className="phone">0974563455</p>
+            <Image url="https://www.terazmuzyka.pl/wp-content/uploads/news/ja/jacksparrow_1057.jpeg"></Image>
+            <p className="truncate w-full text-center name">Philip JohnsonPhilip JohnsonPhilip JohnsonPhilip JohnsonPhilip JohnsonPhilip JohnsonPhilip JohnsonPhilip JohnsonPhilip JohnsonPhilip JohnsonPhilip JohnsonPhilip JohnsonPhilip JohnsonPhilip JohnsonPhilip JohnsonPhilip JohnsonPhilip Johnson</p>
+            <p className="truncate w-full text-center title">Some developer</p>
+            <p className="truncate w-full text-center email">myEmail@gmail.com</p>
+            <p className="truncate w-full text-center phone">0974563455</p>
     </div>
 }
 
